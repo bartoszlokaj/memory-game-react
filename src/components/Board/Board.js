5d6fd81ab280c0e74f5d727d7d1517a4f010f1d3
Bartosz <bartoszlokaj@gmail.com>
2019-09-09 17:43:14 +0200

    outputing cards on board. Working with state

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,16 +4,16 @@ import classes from './Board.module.css';
 import Card from './Card/Card';
 
 const board = props => {
+    const transformedCards = Object.keys(props.cards)
+        .map(igKey => {
+            return [...Array(props.cards[igKey])].map((_, i) => {
+                return <Card key={igKey + i} type={igKey} />
+            });
+        });
+
     return (
         <div className={classes.Board}>
-            <Card type='angular'/>
-            <Card type='vue'/>
-            <Card type='ruby'/>
-            <Card type='react'/>
-            <Card type='js'/>
-            <Card type='css'/>
-            <Card type='html'/>
-            <Card type='sass'/>
+            {transformedCards}
         </div>
     );
 }
--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -4,10 +4,24 @@ import Aux from "../../hoc/Aux";
 import Board from '../../components/Board/Board';
 
 class Game extends Component {
+
+    state = {
+        cards: {
+            html: 2,
+            css: 2,
+            js: 2,
+            react: 2,
+            angular: 2,
+            vue: 2,
+            ruby: 2,
+            sass: 2
+        }
+    }
+
   render() {
     return (
       <Aux>
-        <Board />
+        <Board cards={this.state.cards}/>
         <div>Panel</div>
       </Aux>
     );
