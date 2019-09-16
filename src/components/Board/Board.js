5057ee1281bfdba6ef658bb4a5b38cbb5460e48d
Bartosz <bartoszlokaj@gmail.com>
2019-09-16 14:08:16 +0200

    working on the grid

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import Card from './Card/Card';
-import ScorePanel from '../UI/ScorePanel/ScorePanel';
-import classes from './Board.module.css';
+import Grid from "./Grid/Grid";
+import Card from "./Card/Card";
+import ScorePanel from "../UI/ScorePanel/ScorePanel";
+import classes from "./Board.module.css";
 
 const board = props => {
+  const cards = Array(...props.cards);
+  const cardsDouble = cards.map(el => el);
+  const pairs = cards.concat(cardsDouble);
 
-    const cards = Array(...props.cards);
-    const cardsDouble = cards.map((el) => el);
-    const pairs = cards.concat(cardsDouble);
+  const grid = props.grid;
+  const gridDouble = grid.map(el => el);
+  const gridFinalLength = grid.concat(gridDouble);
+  console.log(gridFinalLength);
 
-    pairs.sort((el1, el2) => Math.random() - Math.random());
+  pairs.sort((el1, el2) => Math.random() - Math.random());  //RANDOMIZING THE ARRAY ELEMENTS
+  
 
-    const transformedPairs = pairs.map((el, index) => {
-        return <Card type={el} key={index} click={() => props.cardClick(el,index)} />;
-    });
-
+  const transformedGrid = pairs.map((el, index) => {
     return (
-        <div className={classes.Board}>
-            {transformedPairs}
-            <ScorePanel/>
-        </div>
+      <Grid key={index} >
+        <Card click={() => props.cardClick(el, index)} type={el}/>
+      </Grid>
     );
-}
+  });
+
+  const transformedPairs = pairs.map((el, index) => {
+    return (
+      <Grid key={index} type={el} click={() => props.cardClick(el, index)} />
+    );
+  });
+
+  return (
+    <div className={classes.Board}>
+      {/* <Aux>{transformedPairs}</Aux> */}
+      {/* {transformedPairs} */}
+      {transformedGrid}
+      <ScorePanel />
+    </div>
+  );
+};
 
 export default board;
--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -6,24 +6,25 @@ import Board from "../../components/Board/Board";
 let PAIR = [];
 let KEYS = [];
 let CARDS = ["html", "css", "angular", "vue", "react", "js", "ruby"];
+let CARDS_COPY = ["html", "css", "angular", "vue", "react", "js", "ruby"];
 
 class Game extends Component {
   state = {
+    grid: CARDS_COPY,
     cards: CARDS,
     score: 0
   };
-
+  
   compareCards = type => {
     return type !== PAIR[0];
   };
-
-
 
   cardClickHandler = (type, key) => {
     KEYS.push(key);
     PAIR.push(type);
     console.log(PAIR);
     console.log(KEYS);
+    console.log(this.state.grid);
 
     if (PAIR[0] === PAIR[1] && KEYS[0] !== KEYS[1]) {
       let cardsFiltered = CARDS.filter(this.compareCards);
@@ -40,7 +41,7 @@ class Game extends Component {
   render() {
     return (
       <Aux>
-        <Board cards={this.state.cards} cardClick={this.cardClickHandler} />
+        <Board grid={this.state.grid} cards={this.state.cards} cardClick={this.cardClickHandler}/>
       </Aux>
     );
   }
